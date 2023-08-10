a20f06ffeb65e9b97620bb84dbdce702f5b495db
z3roco01 <zerocool@e.email>
2023-08-10 00:15:06 -0500

    Add bowls to the creative menu in Food & Drink.

--- src/main/java/z3roco01/waterBowls/item/RegisterItems.java
+++ src/main/java/z3roco01/waterBowls/item/RegisterItems.java
@@ -1,6 +1,7 @@
 package z3roco01.waterBowls.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,5 +15,10 @@ public class RegisterItems {
     public static void register() {
         Registry.register(Registries.ITEM, mkId("water_bowl"), WATER_BOWL);
         Registry.register(Registries.ITEM, mkId("honey_bowl"), HONEY_BOWL);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
+            content.addBefore(Items.MUSHROOM_STEW, HONEY_BOWL);
+            content.addBefore(HONEY_BOWL, WATER_BOWL);
+        });
     }
 }

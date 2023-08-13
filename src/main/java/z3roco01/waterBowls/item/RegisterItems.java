5d9fb810de782f7d5e1bbba57be0a550d4d1436f
z3roco01 <zerocool@e.email>
2023-08-13 16:02:55 -0500

    Add recipe remainder of bowl to the water bowl

--- src/main/java/z3roco01/waterBowls/item/RegisterItems.java
+++ src/main/java/z3roco01/waterBowls/item/RegisterItems.java
@@ -9,7 +9,7 @@ import net.minecraft.registry.Registry;
 import static z3roco01.waterBowls.util.IdUtil.mkId;
 
 public class RegisterItems {
-    public static final Item WATER_BOWL = new Item(new FabricItemSettings());
+    public static final Item WATER_BOWL = new Item(new FabricItemSettings().recipeRemainder(Items.BOWL));
     public static final Item HONEY_BOWL = new HoneyBottleItem(new FabricItemSettings().recipeRemainder(Items.BOWL).food(FoodComponents.HONEY_BOTTLE));
 
     public static void register() {
